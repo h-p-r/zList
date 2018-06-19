6ce07183b593b917ad8969eab8951a092e12c1bb
h-p-r <dev.ac.hpr@gmail.com>
2018-06-19 22:44:09 +0530

    initial

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <Page class="page">
+    <ActionBar class="action-bar" title="Home"/>
+
+    <StackLayout>
+      <Button class="btn btn-primary grad" @tap="$router.push('/counter')">Counter</Button>
+      <Button class="btn btn-primary" @tap="$router.push('/hello')">Hello World</Button>
+    </StackLayout>
+
+  </Page>
+</template>
+
+<style scoped>
+.grad {
+  background: linear-gradient(red,yellow);
+}
+</style>
